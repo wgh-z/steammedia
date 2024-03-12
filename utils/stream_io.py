2524d6a7fb8d09a66ad72d829fce55646a6f1cc4
wgh <3030649311@qq.com>
2024-03-12 22:40:34 +0800

    updated source

--- utils/stream_io.py
+++ utils/stream_io.py
@@ -8,9 +8,9 @@ def push_rtsp_stream(source, stream_id=0):
         '-stream_loop', '-1',  # 循环播放
         '-i', source,
         '-rtsp_transport', 'tcp',  # 传输协议
-        '-bufsize', '1000000k',
-        '-preset:v', 'ultrafast',
-        '-tune:v', 'zerolatency',
+        # '-bufsize', '1000000k',
+        # '-preset:v', 'ultrafast',
+        # '-tune:v', 'zerolatency',
         '-vcodec', 'copy',
         # '-acodec', 'aac',
         '-f', 'rtsp',
@@ -21,10 +21,22 @@ def push_rtsp_stream(source, stream_id=0):
 
 if __name__ == "__main__":
     source = [
-        r'D:\Projects\python\47.Head-count-optimization-mt\Highway-incident\1.mp4',
-        r'D:\Projects\python\47.Head-count-optimization-mt\Highway-incident\2.mp4',
-        r'D:\Projects\python\47.Head-count-optimization-mt\Highway-incident\3.mp4',
-        r'D:\Projects\python\47.Head-count-optimization-mt\Highway-incident\4.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT16\train\MOT16-09.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT16\train\MOT16-10.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT16\train\MOT16-11.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT16\train\MOT16-13.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-01.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-03.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-07.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-08.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-12.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\test\MOT17-14.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-02.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-04.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-09.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-10.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-11.mp4',
+        r'E:\Projects\test_data\video\MOT\MOT17\train\MOT17-13.mp4',
     ]
     for i, s in enumerate(source):
         push_rtsp_stream(s, i)
